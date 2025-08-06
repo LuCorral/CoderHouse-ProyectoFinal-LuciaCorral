dac26ff99539343093762c28fd611b0d523fcaad
LuCorral <86629903+LuCorral@users.noreply.github.com>
2025-08-06 14:26:45 -0300

    Fix date parsing from world clock API

---
+++ FacturacionEntregaProyectoFinal-Corral/src/main/java/com/coderhouse/Service/FechaService.java
@@ -0,0 +1,45 @@
+package com.coderhouse.Service;
+
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import org.springframework.web.client.RestTemplate;
+
+
+
+
+@Service
+public class FechaService {
+	
+    private final static String API_URL = "http://worldclockapi.com/api/json/utc/now";
+
+    public LocalDateTime obtenerFecha() {
+    	LocalDateTime fechaActual;
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<String> response = restTemplate.getForEntity(API_URL, String.class);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(response.getBody());
+            String fecha = jsonNode.path("currentDateTime").asText();
+
+            fechaActual = OffsetDateTime.parse(fecha, DateTimeFormatter.ISO_DATE_TIME).toLocalDateTime();
+            return fechaActual;
+        } catch (Exception e) {
+            // En caso de error al obtener o parsear la fecha remota, se utiliza la fecha local
+            fechaActual = LocalDateTime.now();
+            return fechaActual; // si falla uso lo hago de manewra local
+        }
+    }
+
+
+
+}
